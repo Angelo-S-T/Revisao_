2775cc280bb77a239280bd72ae9e7eb98d41ffee
20232tiimi0160 <angeloestudo30@gmail.com>
2026-03-06 16:29:56 -0300

    Finalizando O projeto, colcando as ligações entra as páginas e acertando os cards

--- app/src/main/java/com/example/revisao/Activity_dashboard.kt
+++ app/src/main/java/com/example/revisao/Activity_dashboard.kt
@@ -1,16 +1,25 @@
 package com.example.revisao
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.revisao.databinding.ActivityDashboardBinding
 
 class Activity_dashboard : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val loginUsuario = intent.getStringExtra("LOGIN_USUARIO") ?:"Desconhecido"
+        binding.txtdescricao.text = "Bem Vindo, $loginUsuario"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/revisao/MainActivity.kt
+++ app/src/main/java/com/example/revisao/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.revisao
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.revisao.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.Login.setOnClickListener {
+            val login = binding.logininput.text.toString().trim()
+            val senha = binding.senhainput.text.toString().trim()
+
+            if(login=="adimin" && senha=="Opaaa"){
+                val intent = Intent(this, Activity_dashboard::class.java)
+                intent.putExtra("LOGIN_USUARIO",login)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this,"login ou senha incorreta",Toast.LENGTH_SHORT)
+            }
         }
+
     }
 }
